a4feff46c4ca13ff52fb1005f0e620f99c073693
Nafiz Alam <nafizalam.dev@gmail.com>
2026-02-08 01:35:28 +0600

    All Problem Done

--- Problem-1.js
+++ Problem-1.js
@@ -1,9 +1,7 @@
 function newPrice(currentPrice, discount) {
     if (typeof currentPrice !== "number" || typeof discount !== "number") {
         return "Invalid"
-    }
-
-    if (discount < 0 || discount > 100) {
+    } else if (discount < 0 || discount > 100) {
         return "Invalid"
     }
 
