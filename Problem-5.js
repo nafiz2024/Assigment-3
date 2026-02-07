e7bd735d1a53891e5e289c89de5fb0c8ba597e43
Nafiz Alam <nafizalam.dev@gmail.com>
2026-02-08 01:33:57 +0600

    Problem-5 Solved

---
+++ Problem-5.js
@@ -0,0 +1,32 @@
+function analyzeText(str) {
+    if (typeof str !== "string") {
+        return "Invalid";
+    }
+
+    if (str.length === 0) {
+        return "Invalid";
+    }
+
+    let noSpaceText = str.split(" ").join("");
+    let tokenCount = noSpaceText.length;
+
+    let words = str.split(" ");
+    let longestWord = words[0];
+
+    for (let word of words) {
+        if (word.length > longestWord.length) {
+            longestWord = word;
+        }
+    }
+
+    return {
+        longwords: longestWord,
+        token: tokenCount,
+    };
+}
+
+
+let result = analyzeText("I am a little honest person");
+let result2 = analyzeText(12345);
+console.log(result); // { wordCount: 6, charCount: 29, averageWordLength: 4.83 }
+console.log(result2); // "Invalid"
