6fadf5cc5ffd0f36168066e60df31776c8d23864
Nafiz Alam <nafizalam.dev@gmail.com>
2026-02-08 00:51:14 +0600

    Problem-2 Solved

---
+++ Problem-2.js
@@ -0,0 +1,20 @@
+function validOtp(otp) {
+    if (typeof otp !== 'string') {
+        return "Invalid";
+    } else if (otp.length !== 8) {
+        return false;
+    } else if (!otp.startsWith("ph-")) {
+        return false;
+    } else {
+        return true;
+    }
+
+
+}
+
+let otp1 = validOtp("ph-12345");
+let otp2 = validOtp("1234");
+let otp3 = validOtp(1234);
+console.log(otp1); // true
+console.log(otp2); // false
+console.log(otp3); // false
