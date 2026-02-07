73df6957860c0f291fc796c6f64f480ed2c93e1b
Nafiz Alam <nafizalam.dev@gmail.com>
2026-02-08 01:14:14 +0600

    Problem-4 Solved

---
+++ Problem-4.js
@@ -0,0 +1,32 @@
+function gonoVote(array) {
+    if (!Array.isArray(array)) {
+        return "Invalid";
+    }
+
+    let haCount = 0;
+    let naCount = 0;
+
+    for (let vote of array) {
+        if (vote === "ha") {
+            haCount++;
+        } else if (vote === "na") {
+            naCount++;
+        }
+    }
+
+    if (haCount > naCount) {
+        return true;
+    } else if (haCount === naCount) {
+        return "equal";
+    } else {
+        return false;
+    }
+        
+}
+
+let result = gonoVote(["ha","na","ha","na"]);
+let result2 = gonoVote(["ha","na","na"]);
+let result3 = gonoVote(["ha","ha","ha","na"]);
+console.log(result); // "tie"
+console.log(result2); // "na"
+console.log(result3); // "ha"   
